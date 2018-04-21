22880bdd8d20f3a16d7f00dc23747d7e151f43da
David Vollmar <david@kabisa.nl>
2018-04-21 12:29:34 +0200

    setup baseline

---
+++ src/game.ts
@@ -0,0 +1,34 @@
+/// <reference path="phaser.d.ts"/>
+
+import "phaser";
+import { MainScene } from "./scenes/mainScene";
+
+// main game configuration
+const config: GameConfig = {
+  width: 800,
+  height: 800,
+  type: Phaser.AUTO,
+  parent: "game",
+  scene: MainScene,
+  input: {
+    mouse: true
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 }
+    }
+  }
+};
+
+// game class
+export class Game extends Phaser.Game {
+  constructor(GameConfig: GameConfig) {
+    super(config);
+  }
+}
+
+// when the page is loaded, create our game instance
+window.onload = () => {
+  var game = new Game(config);
+};
